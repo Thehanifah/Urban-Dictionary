ac29baa03836a1ef12f10b25be6d3125de82966f
The Hanifah <hanifah2015@gmail.com>
2024-07-05 18:06:53 +0100

    fixed bug in input area

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,17 @@ const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
-app.use(cors());
-app.use(bodyParser.json());
+
+const corsOptions = {
+    origin: 'https://urban-dictionary-iota.vercel.app/', //
+    optionsSuccessStatus: 200,
+  };
+
+  app.use(cors(corsOptions));
+  app.use(bodyParser.json());
+
 
 const ADMIN_API_KEY = process.env.ADMIN_API_KEY;
 
--- src/components/AdminLink.jsx
+++ src/components/AdminLink.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';  // Adjust as per your routing setup
 
 function AdminLink() {
   return (
-    <Link to="/admin">Go to Admin Interface</Link>
+    <Link to="http://127.0.0.1:5500/backend/admin.html">Go to Admin Interface</Link>
   );
 }
 
